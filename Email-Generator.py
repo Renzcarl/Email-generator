2871b5e0b7d03b4cec206d1bea0967e5390b7da3
Unknown <109916673+Renzcarl@users.noreply.github.com>
2022-10-06 14:53:39 +0800

    Email-Generator.py

---
+++ Email-Generator.py
@@ -0,0 +1,194 @@
+import time
+import os, sys
+from faker import Faker
+import colorama
+from colorama import Fore,Back,Style
+colorama.init()
+
+os.system("clear")
+print(Fore.GREEN +"       ╔══════════════════════════════════════════╗ ")
+print(Fore.GREEN +"       ║                                                 ║ ")
+print(Fore.GREEN +"       ║  ███████╗███╗░░░███╗░█████╗░██╗██╗░░░░░  ║ ")
+print(Fore.GREEN +"       ║  ██╔════╝████╗░████║██╔══██╗██║██║░░░░░  ║ ")
+print(Fore.GREEN +"       ║  █████╗░░██╔████╔██║███████║██║██║░░░░░  ║ ")
+print(Fore.GREEN +"       ║  ██╔══╝░░██║╚██╔╝██║██╔══██║██║██║░░░░░  ║ ")
+print(Fore.GREEN +"       ║  ███████╗██║░╚═╝░██║██║░░██║██║███████╗  ║ ")
+print(Fore.GREEN +"       ║  ╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚══════╝  ║ ")
+print(Fore.GREEN +"       ║                                                 ║ ")
+print(Fore.GREEN +"       ║            Created by BL3N1N3S                  ║ ")
+print(Fore.GREEN +"       ║                                                 ║ ")
+print(Fore.GREEN +"       ╚══════════════════════════════════════════╝ ")
+
+print("\n════════════════════════════════════════════════════════════════ ")
+
+print("\nAuthor: BLU3N1N3S ")
+print("\nTool Name: Email generator ")
+print("\nFB group: Cyber Phoenix ")
+print("\nNote: this is for help purpose only ")
+print("\nCredits: (Cyber Poenix PH) (BlackCats cyber squad) ")
+print("\nVersion: 0.1 ")
+
+print("\n════════════════════════════════════════════════════════════════\n")
+
+print(" Log in here to Open tool ")
+
+time.sleep(3)
+UserName =input(" [*] USERNAME;\t")
+PassWord =input(" [*] PASSWORD;\t")
+
+
+if UserName == "CPPH2022" and PassWord == "BLU3N1N3S":
+    print(" Sucessfully logged in ")
+        
+else:
+    print(Fore.RED +" [*] log in error maybe username/password incorrect please try again ")
+    exit()
+    
+time.sleep(3)
+os.system("clear")
+print(Fore.GREEN +"       ╔══════════════════════════════════════════╗ ")
+print(Fore.GREEN +"       ║                                                 ║ ")
+print(Fore.GREEN +"       ║  ███████╗███╗░░░███╗░█████╗░██╗██╗░░░░░  ║ ")
+print(Fore.GREEN +"       ║  ██╔════╝████╗░████║██╔══██╗██║██║░░░░░  ║ ")
+print(Fore.GREEN +"       ║  █████╗░░██╔████╔██║███████║██║██║░░░░░  ║ ")
+print(Fore.GREEN +"       ║  ██╔══╝░░██║╚██╔╝██║██╔══██║██║██║░░░░░  ║ ")
+print(Fore.GREEN +"       ║  ███████╗██║░╚═╝░██║██║░░██║██║███████╗  ║ ")
+print(Fore.GREEN +"       ║  ╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚══════╝  ║ ")
+print(Fore.GREEN +"       ║                                                 ║ ")
+print(Fore.GREEN +"       ║            Created by BL3N1N3S                  ║ ")
+print(Fore.GREEN +"       ║                                                 ║ ")
+print(Fore.GREEN +"       ╚══════════════════════════════════════════╝ ")
+
+print("\n════════════════════════════════════════════════════════════════ ")
+
+print("\nAuthor: BLU3N1N3S ")
+print("\nTool Name: Email generator ")
+print("\nFB group: Cyber Phoenix ")
+print("\nNote: this is for help purpose only ")
+print("\nCredits: (Cyber Poenix PH) (BlackCats cyber squad) ")
+print("\nVersion: 0.1 ")
+
+print("\n════════════════════════════════════════════════════════════════\n")
+
+print(Fore.BLUE +" ╔──¤ ◎ script by BLU3N1N3S ")
+print(Fore.BLUE +" │┎─────────────────┐ ")
+print(Fore.BLUE +" │┡─≫ Choose you want    ")
+print(Fore.BLUE +" │└─────────────────┘ ")
+print(Fore.BLUE +" │┌[ 1 ] ≫ random email/password ")
+print(Fore.BLUE +" │├[ 2 ] ≫ visit Facebook page ")
+print(Fore.BLUE +" │├[ 3 ] ≫ Exit tool ")
+print(Fore.BLUE +" │┌────────────────── ")
+Email = input(" └─≫ email/BLU3N1N3S- ")
+
+fake = Faker()
+
+email_password = "1"
+view_fbpg = "2"
+exit_tool = "3"
+
+if Email == email_password:
+    os.system("clear")
+    print(Fore.GREEN +"       ╔══════════════════════════════════════════╗ ")
+    print(Fore.GREEN +"       ║                                                 ║ ")
+    print(Fore.GREEN +"       ║  ███████╗███╗░░░███╗░█████╗░██╗██╗░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  ██╔════╝████╗░████║██╔══██╗██║██║░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  █████╗░░██╔████╔██║███████║██║██║░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  ██╔══╝░░██║╚██╔╝██║██╔══██║██║██║░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  ███████╗██║░╚═╝░██║██║░░██║██║███████╗  ║ ")
+    print(Fore.GREEN +"       ║  ╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚══════╝  ║ ")
+    print(Fore.GREEN +"       ║                                                 ║ ")
+    print(Fore.GREEN +"       ║            Created by BL3N1N3S                  ║ ")
+    print(Fore.GREEN +"       ║                                                 ║ ")
+    print(Fore.GREEN +"       ╚══════════════════════════════════════════╝ ")
+
+    print("\n════════════════════════════════════════════════════════════════ ")
+
+    print("\nAuthor: BLU3N1N3S ")
+    print("\nTool Name: Email generator ")
+    print("\nFB group: Cyber Phoenix ")
+    print("\nNote: this is for help purpose only ")
+    print("\nCredits: (Cyber Poenix PH) (BlackCats cyber squad) ")
+    print("\nVersion: 0.1 ")
+
+    print("\n════════════════════════════════════════════════════════════════\n")
+
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \n[*] Email; ", fake.email())
+    print(" \n[*] Password; ", fake.password())
+    time.sleep(1)
+    print(" \nNote: This account is not created by any user so you can use it to make new account\n ")
+    time.sleep(1)
+    print(" Done. Thanks for using my tool:) ")
+else:
+    print(Fore.RED +" error please try again ")
+
+if Email == view_fbpg:
+    os.system("clear")
+    print(Fore.GREEN +"       ╔══════════════════════════════════════════╗ ")
+    print(Fore.GREEN +"       ║                                                 ║ ")
+    print(Fore.GREEN +"       ║  ███████╗███╗░░░███╗░█████╗░██╗██╗░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  ██╔════╝████╗░████║██╔══██╗██║██║░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  █████╗░░██╔████╔██║███████║██║██║░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  ██╔══╝░░██║╚██╔╝██║██╔══██║██║██║░░░░░  ║ ")
+    print(Fore.GREEN +"       ║  ███████╗██║░╚═╝░██║██║░░██║██║███████╗  ║ ")
+    print(Fore.GREEN +"       ║  ╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚══════╝  ║ ")
+    print(Fore.GREEN +"       ║                                                 ║ ")
+    print(Fore.GREEN +"       ║            Created by BL3N1N3S                  ║ ")
+    print(Fore.GREEN +"       ║                                                 ║ ")
+    print(Fore.GREEN +"       ╚══════════════════════════════════════════╝ ")
+
+    print("\n════════════════════════════════════════════════════════════════ ")
+
+    print("\nAuthor: BLU3N1N3S ")
+    print("\nTool Name: Email generator ")
+    print("\nFB group: Cyber Phoenix ")
+    print("\nNote: this is for help purpose only ")
+    print("\nCredits: (Cyber Poenix PH) (BlackCats cyber squad) ")
+    print("\nVersion: 0.1 ")
+
+    print("\n════════════════════════════════════════════════════════════════\n")
+
+    time.sleep(2)
+    print(" visit this link: https://www.facebook.com/CyberPhoenixPH ")
+else:
+    print(Fore.RED +" error please try again ")
+    
+if Email == exit_tool:
+    print(Fore.RED +" thank you for using my tool ")
+    sys.exit()
+    time.sleep(3)
